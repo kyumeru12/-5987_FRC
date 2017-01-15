ce64f62a8f8ea461ac36d4c0d4b401e4dbf477c5
kyumeru12 <kyumeru12@gmail.com>
2017-01-15 22:03:21 +0200

    Doron

---
+++ src/org/usfirst/frc/team5987/robot/RobotMap.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team5987.robot;
+
+import org.usfirst.frc.team5987.robot.subsystems.ShootingSubsystem;
+
+import edu.wpi.first.wpilibj.DigitalSource;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+	public static final ShootingSubsystem shootingSubsystem = new ShootingSubsystem();
+
+	public static final Subsystem networkTable = null;
+	
+	public static int shootingAnglePort = 0;
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static int leftMotor = 1;
+    // public static int rightMotor = 2;
+	public static int leftShooterPort;
+	public static int rightShooterPort;
+	public static DigitalSource leftShooterChanelA;
+	public static DigitalSource leftShooterChanelB;
+	public static DigitalSource rightShooterChanelA;
+	public static DigitalSource rightShooterChanelB;
+	public static int shooterAngleSensorPort;
+    
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
+}
