617cf71a6dd27e7bada3a3efed690fd40b707e66
kyumeru12 <kyumeru12@gmail.com>
2017-01-25 20:20:22 +0200

    documentation

--- Steamworks_2017/src/org/usfirst/frc/team5987/robot/subsystems/SortingSubsystem.java
+++ Steamworks_2017/src/org/usfirst/frc/team5987/robot/subsystems/SortingSubsystem.java
@@ -17,8 +17,8 @@ public class SortingSubsystem extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-	private Servo sortingServo;
-	public static DigitalInput gears;
+	private Servo sortingServo; //The servo of the subsystem 
+	public static DigitalInput gears; //The limit switch of the the sorting subsystem
 	
 	public SortingSubsystem() {
 		sortingServo = new Servo(RobotMap.sortingServoPort);
@@ -29,19 +29,30 @@ public class SortingSubsystem extends Subsystem {
         // Set the default command for a subsystem here.
         //setDefaultCommand(command);
     }
-    
+    /**
+    *@paprameters position 
+    *the function set position for the servo
+    */
     public void setServoPosition(double position) {
 
     	sortingServo.set(position);
     }
+    /**
+    *@return the function returns the current postion of the servo
+    */
     
     public double getServoPosition() {
     	return sortingServo.get();
     }
     
+	
+    /**
+    *@return the functions return if there is a gear in the subsystem using the limit switch  
+    */
+    
     public boolean isGear() {
-		return !gears.get();
-	}
+	return !gears.get();
+}
     	
     //Approximate will take around 1.25x10^19 years
     public boolean zhoo() {
