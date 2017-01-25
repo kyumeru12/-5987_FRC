4c243511bd9605bd370636abd300776fb8b68662
kyumeru12 <kyumeru12@gmail.com>
2017-01-25 19:14:25 +0200

    Change Dorons mistakeeeee

--- Steamworks_2017/src/org/usfirst/frc/team5987/robot/subsystems/PutGearGroupCommand.java
+++ Steamworks_2017/src/org/usfirst/frc/team5987/robot/subsystems/PutGearGroupCommand.java
@@ -9,10 +9,10 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *@author student
  *@version 1.0v
  *
- *I want to be the vary beast like no one ever was,
- *to catch them is my real test to train them is my pass.
- *I will travel across the land searching for a while,
- *POKEMON to understand the power that inside!!!
+ *I want to be the very best like no one ever was,
+ *to catch them is my real test to train them is cause.
+ *I will travel across the land searching far and wide,
+ *all POKEMON to understand the power that inside!!!
  *POKEMON;
  *
  *------------------------------------------------------------------------------------------
@@ -36,6 +36,8 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *		the answer of 9 multiply by 6 equals to 42!!!
  *
  *------------------------------------------------------------------------------------------
+ *
+ *
  */
 public class PutGearGroupCommand extends CommandGroup {
     
