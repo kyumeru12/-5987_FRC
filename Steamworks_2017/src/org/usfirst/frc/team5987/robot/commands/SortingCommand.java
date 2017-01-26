f78d372b8d9f44c1fe6dee9340007ae4e519f51b
kyumeru12 <kyumeru12@gmail.com>
2017-01-26 17:13:52 +0200

    Update SortingCommand.java

--- Steamworks_2017/src/org/usfirst/frc/team5987/robot/commands/SortingCommand.java
+++ Steamworks_2017/src/org/usfirst/frc/team5987/robot/commands/SortingCommand.java
@@ -42,6 +42,7 @@ public class SortingCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+	    Timer.delay(0.05);
     }
 
     // Make this return true when this Command no longer needs to run execute()
